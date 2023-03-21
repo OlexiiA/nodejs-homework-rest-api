7b2a407c4593be3c6d8c91d8ef176c318ae29bf9
OlexiiA <incretiblealex@gmail.com>
2023-03-21 19:09:17 +0200

    add resizing avatar

--- controllers/currentUser/updateAvatar.js
+++ controllers/currentUser/updateAvatar.js
@@ -1,6 +1,7 @@
 const { User } = require("../../models/users");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -13,12 +14,16 @@ const updateAvatar = async (req, res) => {
     const resultUpload = path.join(avatarsDir, imageName);
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("public", "avatars", imageName);
-    await User.findByIdAndUpdate(req.user._id, { avatarURL });
+    
+    const image = await Jimp.read(resultUpload);
+    await image.resize(250, 250);
+    await image.write(resultUpload);
+    await User.findByIdAndUpdate(req.user._id, { image: avatarURL });
     res.json({ avatarURL });
+
   } catch (error) {
     await fs.unlink(tempUpload);
     throw error;
   }
 };
-
 module.exports = updateAvatar;
